5b89c6f938f1d4c2a152505b3c74753718769f23
azizsiler <azizsiler@gmail.com>
2022-05-22 20:26:01 +0300

    220522

---
+++ src/staticKeyword/C03.java
@@ -0,0 +1,14 @@
+package staticKeyword;
+
+public class C03 {
+
+    static int sayi=5;
+
+
+    public static void artirma(){
+
+        sayi++;
+        System.out.println("sayinin C03 deki degeri : " + sayi);
+    }
+
+}
